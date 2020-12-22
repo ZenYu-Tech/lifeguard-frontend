395742e848fd8aadb43e1ff296591869c63c3456
miayang0513 <a16168972@gmail.com>
2020-12-22 11:38:12 +0800

    chore: install element-ui and configure it

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,14 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    { src: 'element-ui/lib/theme-chalk/index.css' }
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '@/plugins/apis/axios'
+    '@/plugins/apis/axios',
+    '@/plugins/element-ui'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -31,5 +34,7 @@ export default {
   modules: ['@nuxtjs/axios'],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    transpile: [/^element-ui/],
+  },
 }
