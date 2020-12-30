68cc99aadf22d5e3e9ede75182bc79fa1af7be8a
miayang0513 <a16168972@gmail.com>
2020-12-31 06:27:41 +0800

    feat: layout section in home page, and add shared element CardNews

---
+++ components/CardNews.vue
@@ -0,0 +1,86 @@
+<template>
+  <article class="news">
+    <div class="news__header">
+      <h6 class="news__title">{{ news.title }}</h6>
+      <time class="news__time">{{ dateFormator(news.createdAt) }}</time>
+    </div>
+    <p class="news__content">{{ news.content }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'CardNews',
+  props: {
+    news: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    dateFormator(date) {
+      return new Date(date).toISOString().slice(0, 10)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.news {
+  &__header {
+    display: grid;
+    grid-template-columns: 14px 1fr 83px;
+    grid-template-rows: 25px;
+    column-gap: 5px;
+    align-items: center;
+    &::before {
+      position: relative;
+      content: '';
+      display: block;
+      width: 14px;
+      height: 14px;
+      border-radius: 50%;
+      background-color: map-get($map: $colors, $key: primary);
+    }
+  }
+  &__title {
+    font-family: Noto Sans TC;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 25px;
+  }
+  &__time {
+    font-family: Noto Sans TC;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 25px;
+    color: #a9abb3;
+  }
+  &__content {
+    margin-top: 16px;
+    padding: 13px 19px;
+    background-color: map-get($map: $colors, $key: primary);
+    border-radius: 5px;
+    font-size: 18px;
+    line-height: 26px;
+    color: #ffffff;
+    position: relative;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0px;
+      transform: translateY(-100%);
+      left: 18px;
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 0 10px 12px 10px;
+      border-color: transparent transparent map-get($map: $colors, $key: primary) transparent;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,113 @@
 <template>
-  <el-row>
-    <el-col :style="{ 'background-image': `url(${require('@/assets/images/banner.png')})`, height: '787px' }"></el-col>
-  </el-row>
+  <main>
+    <section class="banner" :style="{ 'background-image': `url(${require('@/assets/images/banner.png')})` }"></section>
+    <div class="section__wrapper">
+      <section class="section__container__news">
+        <h3 class="section__title">最新消息</h3>
+        <card-news v-for="news in getArticlesByCategory('news')" :key="news.id" :news="news"></card-news>
+      </section>
+      <section class="section__container__training">
+        <h3 class="section__title">訓練影片</h3>
+      </section>
+      <section class="section__container__highlight">
+        <h3 class="section__title">活動花絮</h3>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'Home'
+  name: 'Home',
+  computed: {
+    ...mapGetters({
+      getArticlesByCategory: 'article/getArticlesByCategory'
+    })
+  },
+  created() {
+    console.log(this.getArticlesByCategory('news'))
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  width: 100vw;
+  height: 667px;
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: cover;
+}
+
+.section {
+  &__wrapper {
+    padding: 50px 17px;
+  }
+  &__container {
+    &__news {
+      background: #f2f5f9;
+      border-radius: 5px;
+      display: grid;
+      grid-auto-flow: row;
+      grid-auto-rows: max-content;
+      row-gap: 26px;
+      padding: 40px 12px 30px 12px;
+      position: relative;
+      & > .section__title {
+        position: absolute;
+        left: 12px;
+        top: 0px;
+        transform: translateY(-50%);
+      }
+    }
+  }
+  &__title {
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 32px;
+    color: #2f2f2f;
+    position: relative;
+    .section__container__training > &:before,
+    .section__container__highlight > &:before {
+      content: '';
+      position: absolute;
+      width: 40px;
+      height: 40px;
+      top: -14px;
+      left: -14px;
+      z-index: -1;
+      background-color: rgba($color: map-get($map: $colors, $key: primary), $alpha: 0.7);
+      border-radius: 50%;
+    }
+  }
+
+  @media (min-width: 768px) {
+    &__wrapper {
+      padding: 70px 38px;
+    }
+    &__container {
+      &__news {
+        width: 57%;
+        padding: 40px 28px 30px 28px;
+      }
+    }
+    &__title {
+      font-size: 40px;
+      line-height: 40px;
+      .section__container__training > &:before,
+      .section__container__highlight > &:before {
+        width: 50px;
+        height: 50px;
+        top: -20px;
+        left: -20px;
+      }
+    }
+  }
+  @media (min-width: 1200px) {
+    &__wrapper {
+      padding: 80px 100px;
+    }
+  }
+}
+</style>
