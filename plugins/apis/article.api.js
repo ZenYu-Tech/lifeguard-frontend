003c2f0e01b92f89c9f5e72191e46c8a4f6f664d
miayang0513 <a16168972@gmail.com>
2021-01-01 09:12:42 +0800

    fix: revise aritcle related api path and add \!important attribute when hover menu item

--- plugins/apis/article.api.js
+++ plugins/apis/article.api.js
@@ -1,11 +1,11 @@
 export default axios => ({
   fetchArticle(category, id) {
-    return axios(`/articles/${category}/${id}`, {
+    return axios(`/article/${category}/${id}`, {
       method: 'GET'
     })
   },
   fetchArticles(category) {
-    return axios(`/articles/${category}`, {
+    return axios(`/article/${category}`, {
       method: 'GET'
     })
   }
