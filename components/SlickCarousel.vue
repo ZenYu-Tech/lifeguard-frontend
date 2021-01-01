49739b9c744eca111c7e902f36244e35489236e4
miayang0513 <a16168972@gmail.com>
2021-01-01 09:14:07 +0800

    feat: install vue-slick-carousel and wrap a component to reuse

---
+++ components/SlickCarousel.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :style="style">
+    <client-only>
+      <vue-slick-carousel v-bind="options">
+        <slot></slot>
+      </vue-slick-carousel>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+  name: 'SlickCarousel',
+  components: {
+    VueSlickCarousel
+  },
+  props: {
+    width: {
+      type: [Number, String],
+      default: 'auto'
+    },
+    height: {
+      type: [Number, String],
+      default: 'auto'
+    },
+    options: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    style() {
+      return {
+        width: this.convertToUnit(this.width),
+        height: this.convertToUnit(this.height)
+      }
+    }
+  },
+  methods: {
+    convertToUnit(str, unit = 'px') {
+      if (str == null || str === '') {
+        return undefined
+      } else if (isNaN(str)) {
+        return String(str)
+      } else {
+        return `${Number(str)}${unit}`
+      }
+    }
+  }
+}
+</script>
