ceda7da8f7603399224c14d17424fbf87ed8637c
miayang0513 <a16168972@gmail.com>
2021-01-16 22:30:37 +0800

    feat(admin): add admin article api interface

--- plugins/apis/article.api.js
+++ plugins/apis/article.api.js
@@ -8,5 +8,41 @@ export default axios => ({
     return axios(`/article/${category}/?count=${count}&page=${page}`, {
       method: 'GET'
     })
+  },
+  admin: {
+    fetchArticles(count = 10, page = 1) {
+      return axios(`/manage/article/?count=${count}&page=${page}`, {
+        method: 'GET'
+      })
+    },
+    createArticle(category, formData) {
+      return axios(`/manage/article/${category}`, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        method: 'POST',
+        data: formData
+      })
+    },
+    fetchArticle(category, articleId) {
+      return axios(`/manage/article/${category}/${articleId}`, {
+        method: 'GET'
+      })
+    },
+    editArticle(category, articleId, formData) {
+      return axios(`/manage/article/${category}/${articleId}`, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        method: 'PUT',
+        data: formData
+      })
+    },
+    deleteArticle(category, articleId) {
+      return axios(`/manage/article/${category}/${articleId}`, {
+        method: 'DELETE'
+      })
+    },
+    deleteImage(category, articleId, imageId) {
+      return axios(`/manage/article/${category}/${articleId}/${imageId}`, {
+        method: 'DELETE'
+      })
+    }
   }
 })
---
+++ store/admin/article.js
@@ -0,0 +1,115 @@
+/**
+ * @typeof Article
+ * @type {object}
+ * @property {string} articleId
+ * @property {string} category
+ * @property {string} title
+ * @property {string} content
+ * @property {number} sort
+ * @property {boolean} show
+ * @property {date} createdAt
+ * @property {date} updatedAt
+ */
+
+/**
+ * @typeof Image
+ * @type {object}
+ * @property {string} imageId
+ * @property {boolean} main
+ * @property {base64} image
+ */
+
+const state = () => ({
+  /**
+   * @type {Array<Article>}
+   */
+  articles: [],
+  article: {
+    articleId: '1',
+    title: 'title',
+    content: '<html>Hello world 31</html>',
+    /**
+     * @type {Array<Image>}
+     */
+    images: []
+  }
+})
+
+const getters = {
+  getArticles: state => state.articles,
+  getArticlesByCategory: state => category => {
+    return state.articles.filter(article => article.category === category)
+  },
+  getArticle: state => state.article,
+  getArticleById: state => id => {
+    return state.articles.find(article => article.articleId === id)
+  }
+}
+
+const mutations = {
+  SET_article(state, article) {
+    state.article = article
+  },
+  SET_articles(state, articles) {
+    state.articles = articles
+  }
+}
+
+const actions = {
+  async fetchArticle({ commit }, { category, articleId }) {
+    try {
+      const { data } = await this.$articleApi.admin.fetchArticle(category, articleId)
+      commit('SET_article', data.result)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async fetchArticles({ commit }, { count = 10, page = 1 }) {
+    try {
+      const { data } = await this.$articleApi.admin.fetchArticles(count, page)
+      commit('SET_articles', data.result.articles)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async createArticle(context, { category, title, content, mainImageIndex, images }) {
+    try {
+      const formData = new FormData()
+      formData.append('title', title)
+      formData.append('content', content)
+      formData.append('mainImageIndex', mainImageIndex)
+      formData.append('images', images)
+      await this.$articleApi.admin.createArticle(category, formData)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async editArticle(context, { category, articleId, title, content, mainImageIndex, images }) {
+    try {
+      const formData = new FormData()
+      formData.append('title', title)
+      formData.append('content', content)
+      formData.append('mainImageIndex', mainImageIndex)
+      formData.append('images', images)
+      await this.$articleApi.admin.editArticle(category, articleId, formData)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async deleteArticle(context, { category, articleId }) {
+    try {
+      await this.$articleApi.admin.deleteArticle(category, articleId)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async deleteImage(context, { category, articleId, imageId }) {
+    try {
+      await this.$articleApi.admin.deleteImage(category, articleId, imageId)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export { state, getters, mutations, actions }
