8b0b92b2699187924c8ed75f7475b0bae1dd856f
miayang0513 <a16168972@gmail.com>
2021-01-16 22:51:09 +0800

    feat(admin): add admin video api interface

--- plugins/apis/video.api.js
+++ plugins/apis/video.api.js
@@ -3,5 +3,31 @@ export default axios => ({
     return axios(`/video`, {
       method: 'GET'
     })
+  },
+  admin: {
+    fetchVideos(count, page) {
+      return axios(`/manage/video/?count=${count}&page=${page}`, {
+        method: 'GET'
+      })
+    },
+    createVideo(title, embedIframe) {
+      return axios(`/manage/video`, {
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        method: 'POST',
+        data: { title, embedIframe }
+      })
+    },
+    editVideo(videoId, title, embedIframe) {
+      return axios(`/manage/video/${videoId}`, {
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        method: 'PUT',
+        data: { title, embedIframe }
+      })
+    },
+    deleteVideo(videoId) {
+      return axios(`/manage/video/${videoId}`, {
+        method: 'DELETE'
+      })
+    }
   }
 })
---
+++ store/admin/video.js
@@ -0,0 +1,61 @@
+/**
+ * @typeof Video
+ * @type {object}
+ * @property {string} videoId
+ * @property {string} title
+ * @property {string} embedIframe
+ * @property {number} sort
+ * @property {date} createdAt
+ * @property {base64} image
+ */
+
+const state = () => ({
+  /**
+   * @type {Array<Video>}
+   */
+  videos: []
+})
+
+const getters = {
+  getVideos: state => state.videos.slice().sort((a, b) => a.sort - b.sort)
+}
+
+const mutations = {
+  SET_videos(state, videos) {
+    state.videos = videos
+  }
+}
+
+const actions = {
+  async fetchVideos({ commit }, { count, page }) {
+    try {
+      const { data } = await this.$videoApi.admin.fetchVideos(count, page)
+      commit('SET_videos', data.result.videos)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async createVideo(context, { title, embedIframe }) {
+    try {
+      await this.$videoApi.admin.createVideo(title, embedIframe)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async editVideo(context, { videoId, title, embedIframe }) {
+    try {
+      await this.$videoApi.admin.editVideo(videoId, title, embedIframe)
+    } catch (error) {
+      console.error(error)
+    }
+  },
+  async deleteVideo(context, { videoId }) {
+    try {
+      await this.$videoApi.admin.deleteVideo(videoId)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export { state, getters, mutations, actions }
