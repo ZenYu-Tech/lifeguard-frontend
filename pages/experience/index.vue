7834172f62f295149a5531218f6c2cd31625e607
miayang0513 <a16168972@gmail.com>
2021-01-28 00:29:47 +0800

    feat: inifinite scroll

--- pages/experience/index.vue
+++ pages/experience/index.vue
@@ -3,9 +3,9 @@
     <section class="experience">
       <h3 class="experience__title">活動花絮</h3>
       <div class="experience__wrapper">
-        <template v-for="article in getArticlesByCategory('experience')">
+        <template v-for="(article, index) in getArticlesByCategory('experience')">
           <nuxt-link
-            :key="article.articleId"
+            :key="`${index}-${article.articleId}`"
             tag="div"
             :to="`/experience/${article.articleId}`"
             class="experience__item"
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Experience',
@@ -28,12 +28,52 @@ export default {
     haveBanner: false
   },
   async asyncData({ store }) {
-    await store.dispatch('client/article/fetchArticles', { category: 'experience' })
+    const category = 'experience'
+    const count = 12
+    await store.dispatch('client/article/fetchArticles', { category, count })
+    return { category, count }
+  },
+  data() {
+    return {
+      category: '',
+      count: 0
+    }
   },
   computed: {
     ...mapGetters('client', {
-      getArticlesByCategory: 'article/getArticlesByCategory'
+      getArticlesByCategory: 'article/getArticlesByCategory',
+      getPagination: 'article/getPagination'
     })
+  },
+  mounted() {
+    window.onscroll = this.infiniteScroll
+    this.$once('hook:beforeDestroy', () => {
+      window.onscroll = null
+    })
+    // Call the function first to avoid origin window being too height to never trigger scroll event
+    this.infiniteScroll()
+  },
+  methods: {
+    ...mapActions('client', {
+      fetchArticles: 'article/fetchArticles'
+    }),
+    async infiniteScroll() {
+      const footerHeight = document.querySelector('.footer').clientHeight
+      const totalHeight = document.documentElement.scrollHeight
+      const windowHeight = window.innerHeight
+      const scrollTop = document.documentElement.scrollTop
+
+      if (totalHeight - footerHeight <= windowHeight + scrollTop) {
+        const { page, totalPage } = this.getPagination
+        if (page === totalPage) {
+          return
+        }
+        if (page !== totalPage) {
+          await this.fetchArticles({ category: this.category, count: this.count, page: page + 1 })
+        }
+        this.infiniteScroll()
+      }
+    }
   }
 }
 </script>
